b47bb2d42146281505fe5a48313821265e0bdb6f
Pail4 <73517684+Pail4@users.noreply.github.com>
2022-05-28 12:50:44 +0400

    eslint, ts configs

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    semi: [2, 'always'],
+    'space-before-function-paren': [2, {
+      anonymous: 'never',
+      named: 'never',
+      asyncArrow: 'never'
+    }],
+    indent: ['warn', 2],
+    'react/prop-types': [0]
+  }
+};
