6527350c3c61653a9cfb2733e265226c42ba5fdf
Pail4 <73517684+Pail4@users.noreply.github.com>
2022-05-29 15:25:04 +0400

    likeBtn bug fixed, doubleClick like added

--- src/components/Block/-LikeBtn/LikeBtn.tsx
+++ src/components/Block/-LikeBtn/LikeBtn.tsx
@@ -1,11 +1,18 @@
-import './LikeBtn.css';
+import "./LikeBtn.css";
 
-import React from 'react';
+import React from "react";
 
-export function LikeBtn({ onClick, isLiked }: {onClick: VoidFunction, isLiked: boolean}): JSX.Element {
+export function LikeBtn({
+  onClick,
+  isLiked,
+}: {
+  onClick: VoidFunction;
+  isLiked: boolean;
+}): JSX.Element {
   return (
-    <button className={"Block-LikeBtn " + (isLiked ? "Block-LikeBtn_liked" : "")} onClick={onClick}>
-      
-    </button>
+    <button
+      className={"Block-LikeBtn " + (isLiked ? "Block-LikeBtn_liked" : "")}
+      onClick={onClick}
+    ></button>
   );
 }
--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,13 +1,14 @@
-import './Block.css';
+import "./Block.css";
 
-import React from 'react';
-import { LikeBtn } from './-LikeBtn/LikeBtn';
-import { cat } from '../../store/interfaces';
-import { useDispatch } from 'react-redux';
-import { dislikeCat, likeCat } from '../../store/reducers';
+import React, { useState } from "react";
+import { LikeBtn } from "./-LikeBtn/LikeBtn";
+import { cat } from "../../store/interfaces";
+import { useDispatch } from "react-redux";
+import { dislikeCat, likeCat } from "../../store/reducers";
 
 export function Block({ cat }: { cat: cat }): JSX.Element {
   const dispatch = useDispatch();
+  const [lastTap, setLastTap] = useState(new Date());
   function likeClick() {
     if (!cat.isLiked) {
       dispatch(likeCat(cat));
@@ -16,8 +17,26 @@ export function Block({ cat }: { cat: cat }): JSX.Element {
     }
   }
 
+  function doubletap() {
+    const now = new Date();
+    const timesince = now.getTime() - lastTap.getTime();
+    if (timesince < 300 && timesince > 0) {
+      likeClick();
+    }
+    setLastTap(new Date());
+  }
+
   return (
-    <div className="Block" style={{backgroundImage: `url(${cat.image})`}}>
+    <div
+      className="Block"
+      onLoad={() => console.log("loading...")}
+      onClick={doubletap}
+    >
+      <img
+        src={cat.image.toString()}
+        alt="sweety-kitty"
+        className="Block-Img"
+      />
       <LikeBtn onClick={likeClick} isLiked={cat.isLiked} />
     </div>
   );
